492c48f19b4360119b4bbaa9806c7f137e938b68
ktynski <kristin@frac.tl>
2023-05-17 12:16:11 -0400

    Update run.py

--- run.py
+++ run.py
@@ -287,22 +287,33 @@ def generate_content(prompt, model="gpt-3.5-turbo", max_tokens=1000, temperature
     return None
 
 
+
 def generate_semantic_improvements_guide(prompt,query, model="gpt-3.5-turbo", max_tokens=2000, temperature=0.4):
-    gpt_response = openai.ChatCompletion.create(
-        model=model,
-        messages=[
-            {"role": "system", "content": "You are an expert at Semantic SEO. In particular, you are superhuman at taking the result of an NLP keyword analysis of a search engine results page for a given keyword, and using it to build a readout/guide that can be used to inform someone writing a long-form article about a given topic so that they can best fully cover the semantic SEO as shown in the SERP. The goal of this guide is to help the writer make sure that the content they are creating is as comprehensive to the semantic SEO expressed in the content that ranks on the first page of Google for the given query. With the following semantic data, please provide this readout/guide. This readout/guide should be useful to someone writing about the topic, and should not include instructions to add info to the article about the SERP itself. The SERP semantic SEO data is just to be used to help inform the guide/readout. Please provide the readout/guide in well organized and hierarchical."},
-            {"role": "user", "content": f"Semantic SEO data for the keyword based on the content that ranks on the first page of google for the given keyword query of: {query} and it's related semantic data:  {prompt}"}],
-        max_tokens=max_tokens,
-        n=1,
-        stop=None,
-        temperature=temperature,
-    )
-    response = gpt_response['choices'][0]['message']['content'].strip()
-    #print(response)
-    formatted_response = response.strip().split('\n')
-    #save_to_file("Semantic_SEO_Readout.txt", formatted_response)
-    return str(response)   
+    for i in range(3):
+        try:
+            gpt_response = openai.ChatCompletion.create(
+                model=model,
+                messages=[
+                    {"role": "system", "content": "You are an expert at Semantic SEO. In particular, you are superhuman at taking the result of an NLP keyword analysis of a search engine results page for a given keyword, and using it to build a readout/guide that can be used to inform someone writing a long-form article about a given topic so that they can best fully cover the semantic SEO as shown in the SERP. The goal of this guide is to help the writer make sure that the content they are creating is as comprehensive to the semantic SEO expressed in the content that ranks on the first page of Google for the given query. With the following semantic data, please provide this readout/guide. This readout/guide should be useful to someone writing about the topic, and should not include instructions to add info to the article about the SERP itself. The SERP semantic SEO data is just to be used to help inform the guide/readout. Please provide the readout/guide in well organized and hierarchical."},
+                    {"role": "user", "content": f"Semantic SEO data for the keyword based on the content that ranks on the first page of google for the given keyword query of: {query} and it's related semantic data:  {prompt}"}],
+                max_tokens=max_tokens,
+                n=1,
+                stop=None,
+                temperature=temperature,
+            )
+            response = gpt_response['choices'][0]['message']['content'].strip()
+            formatted_response = response.strip().split('\n')
+            st.markdown(formatted_response)
+            return str(response)
+
+        except openai.api_resources.request_error.RequestError as e:
+            print(f"Attempt {i+1} failed, retrying...")
+            time.sleep(3)  # Wait for 3 seconds before next try
+
+    st.write("OpenAI is currently overloaded, please try again later.")
+    return None
+    
+   
 
 def generate_outline(topic, model="gpt-3.5-turbo", max_tokens=1000):
     prompt = f"Generate an incredibly thorough article outline for the topic: {topic}. Consider all possible angles and be as thorough as possible. Please use Roman Numerals for each section."
